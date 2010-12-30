ccb6204c3f16117785fbc80e07c7fd8945ec6eec
Plamen Tanov <1.plamen@gmail.com>
2010-12-30 14:46:15 +0200

    (#28) Technique pages are added only if CustomCategory is Technique, not to every CustomCategory.

--- core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/extension/TechniquePageProviderExtension.java
+++ core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/extension/TechniquePageProviderExtension.java
@@ -4,17 +4,17 @@ import java.util.Map;
 
 import org.eclipse.epf.authoring.ui.forms.ContentElementGuidancePage;
 import org.eclipse.epf.authoring.ui.providers.IMethodElementEditorPageProviderExtension;
-import org.eclipse.epf.uma.CustomCategory;
 import org.eclipse.ui.forms.editor.FormEditor;
 
 import eu.tanov.epf.pv.techniques.pages.TechniqueTasksPage;
 import eu.tanov.epf.pv.techniques.pages.TechniqueWorkProductsPage;
+import eu.tanov.epf.pv.techniques.util.TechniquesHelper;
 
 public class TechniquePageProviderExtension implements IMethodElementEditorPageProviderExtension {
 
 	@Override
 	public Map<Object, String> getPages(Map<Object, String> pageMap, FormEditor editor, Object input) {
-		if (input instanceof CustomCategory) {
+		if (TechniquesHelper.isTechnique(input)) {
 			pageMap.put(new TechniqueTasksPage(editor), null);
 			pageMap.put(new TechniqueWorkProductsPage(editor), null);
 			pageMap.put(new ContentElementGuidancePage(editor), null);
