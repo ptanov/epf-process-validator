4d4b7c4b30937dbb629d66b470be5753b4c16faa
Plamen Tanov <1.plamen@gmail.com>
2010-12-30 23:00:00 +0200

    (#30) Removed variability section - it searches in custom category, not in techniques

--- core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/pages/TechniqueDescriptionPage.java
+++ core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/pages/TechniqueDescriptionPage.java
@@ -1,6 +1,8 @@
 package eu.tanov.epf.pv.techniques.pages;
 
 import org.eclipse.epf.authoring.ui.forms.CustomCategoryDescriptionPage;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.forms.editor.FormEditor;
 
 import eu.tanov.epf.pv.techniques.i18n.TechniquesUIResources;
@@ -9,6 +11,15 @@ public class TechniqueDescriptionPage extends CustomCategoryDescriptionPage {
 
 	public TechniqueDescriptionPage(FormEditor editor) {
 		super(editor);
+	}
+	
+	@Override
+	public void init(IEditorSite site, IEditorInput input) {
+		super.init(site, input);
+		
+		//without variability - if on - it searches in custom categories, not in techniques
+		//but it is not used - so disable
+		variabilitySectionOn = false;
 	}
 	
 	@Override
