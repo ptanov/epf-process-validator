335544b2163b6e323115536567729e1b79d2bd21
Plamen Tanov <1.plamen@gmail.com>
2010-12-31 00:31:29 +0200

    (#30) Form title "Custom Category" replaced by "Technique" for Guidance page

--- core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/pages/TechniqueGuidancePage.java
+++ core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/pages/TechniqueGuidancePage.java
@@ -1,16 +1,61 @@
 package eu.tanov.epf.pv.techniques.pages;
 
 import org.eclipse.epf.authoring.ui.forms.ContentElementGuidancePage;
+import org.eclipse.epf.library.edit.util.TngUtil;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 
 import eu.tanov.epf.pv.techniques.i18n.TechniquesUIResources;
+import eu.tanov.epf.pv.techniques.util.FormHelper;
 
 public class TechniqueGuidancePage extends ContentElementGuidancePage {
-
+	private static final String classNameForFormTitle = TechniquesUIResources.technique_text;
+	
 	public TechniqueGuidancePage(FormEditor editor) {
 		super(editor);
 	}
 
+	@Override
+	protected void createFormContent(IManagedForm managedForm) {
+		super.createFormContent(managedForm);
+
+		FormHelper.updateFormText(form, classNameForFormTitle, contentElement);
+	}
+
+	@Override
+	protected void addListeners() {
+		super.addListeners();
+
+		FormHelper.replaceLastListener(form, SWT.Activate, new Listener() {
+			public void handleEvent(Event e) {
+				FormHelper.updateFormText(form, classNameForFormTitle, contentElement);
+
+				refreshViewers();
+				if (TngUtil.isLocked(contentElement)) {
+					enableControls(false);
+				} else {
+					enableControls(true);
+				}
+			}
+		}, "org.eclipse.epf.authoring.ui.forms.AssociationFormPage");
+
+		FormHelper.replaceLastListener(form, SWT.Deactivate, new Listener() {
+			public void handleEvent(Event e) {
+				FormHelper.updateFormText(form, classNameForFormTitle, contentElement);
+
+				refreshViewers();
+				if (TngUtil.isLocked(contentElement)) {
+					enableControls(false);
+				} else {
+					enableControls(true);
+				}
+			}
+		}, "org.eclipse.epf.authoring.ui.forms.AssociationFormPage");
+	}
+	
 	@Override
 	protected String getSectionDescription() {
 		return TechniquesUIResources.technique_guidancepage_sectiondescription;
