1e8a72646b4527f207e87d93a27ea93f3e16e745
Plamen Tanov <1.plamen@gmail.com>
2010-12-30 14:52:31 +0200

    (#26) Assign page is removed from Technique

--- core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/extension/TechniquePageProviderExtension.java
+++ core/eu.tanov.epf.pv.techniques/src/eu/tanov/epf/pv/techniques/extension/TechniquePageProviderExtension.java
@@ -1,8 +1,10 @@
 package eu.tanov.epf.pv.techniques.extension;
 
+import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.epf.authoring.ui.forms.ContentElementGuidancePage;
+import org.eclipse.epf.authoring.ui.forms.CustomCategoryAssignPage;
 import org.eclipse.epf.authoring.ui.providers.IMethodElementEditorPageProviderExtension;
 import org.eclipse.ui.forms.editor.FormEditor;
 
@@ -18,9 +20,22 @@ public class TechniquePageProviderExtension implements IMethodElementEditorPageP
 			pageMap.put(new TechniqueTasksPage(editor), null);
 			pageMap.put(new TechniqueWorkProductsPage(editor), null);
 			pageMap.put(new ContentElementGuidancePage(editor), null);
+			
+			removeAssignPage(pageMap);
 		}
 
 		return pageMap;
+	}
+
+	private void removeAssignPage(Map<Object, String> pageMap) {
+		for (Iterator<Object> iterator = pageMap.keySet().iterator(); iterator.hasNext();) {
+			final Object key = iterator.next();
+			if (key instanceof CustomCategoryAssignPage) {
+				iterator.remove();
+				//we assume that only one AssignPage is added - so break in first match
+				break;
+			}
+		}
 	}
 
 }
