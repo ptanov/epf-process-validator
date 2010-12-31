0ae32b8cfc9a59cc13f9b579cd75c3423b1f3093
Plamen Tanov <1.plamen@gmail.com>
2010-12-31 17:13:59 +0200

    (#46) TngUtil is used Complete: http://code.google.com/p/epf-process-validator/issues/detail?id=46

--- core/eu.tanov.epf.pv.ui.common/src/eu/tanov/epf/pv/ui/common/util/FilteredContentElementOrderList.java
+++ core/eu.tanov.epf.pv.ui.common/src/eu/tanov/epf/pv/ui/common/util/FilteredContentElementOrderList.java
@@ -1,11 +1,11 @@
 package eu.tanov.epf.pv.ui.common.util;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.epf.library.edit.util.ContentElementOrderList;
+import org.eclipse.epf.library.edit.util.TngUtil;
 import org.eclipse.epf.uma.ContentElement;
 import org.eclipse.epf.uma.VariabilityElement;
 
@@ -28,7 +28,7 @@ public class FilteredContentElementOrderList<T> extends ContentElementOrderList 
 	 */
 	private final EStructuralFeature feature;
 
-	private final Class<?> acceptOnlyType;
+	private final Class<T> acceptOnlyType;
 
 	public FilteredContentElementOrderList(ContentElement contentElement, EStructuralFeature feature, Class<T> acceptOnlyType) {
 		super(contentElement, CONTENT_ELEMENTS__FOR_ELEMENT_ONLY, feature);
@@ -45,14 +45,7 @@ public class FilteredContentElementOrderList<T> extends ContentElementOrderList 
 		final List<T> result;
 		final Object object = editElement.eGet(feature);
 		if (object instanceof List) {
-			result = new ArrayList<T>();
-			for (Object next : (List<?>) object) {
-				if (acceptOnlyType.isInstance(next)) {
-					@SuppressWarnings("unchecked")
-					final T casted = (T) next;
-					result.add(casted);
-				}
-			}
+			result = TngUtil.<T> extractType((List<?>) object, acceptOnlyType);
 		} else if (object == null) {
 			result = Collections.emptyList();
 		} else {
