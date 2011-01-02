bd3e36bf744902948de31f419d52465f1ae6c782
Plamen Tanov <1.plamen@gmail.com>
2011-01-02 15:58:20 +0200

    (#42) Work products are updated when tasks are modified through UI http://code.google.com/p/epf-process-validator/issues/detail?id=42, Complete

--- core/eu.tanov.epf.pv.ui.techniques/src/eu/tanov/epf/pv/ui/techniques/pages/TechniqueTasksPage.java
+++ core/eu.tanov.epf.pv.ui.techniques/src/eu/tanov/epf/pv/ui/techniques/pages/TechniqueTasksPage.java
@@ -1,13 +1,17 @@
 package eu.tanov.epf.pv.ui.techniques.pages;
 
+import java.util.List;
+
 import org.eclipse.epf.authoring.ui.AuthoringUIText;
 import org.eclipse.epf.library.edit.itemsfilter.FilterConstants;
 import org.eclipse.epf.uma.Task;
 import org.eclipse.ui.forms.editor.FormEditor;
 
 import eu.tanov.epf.pv.ui.common.pages.AbstractCustomCategoryPage;
+import eu.tanov.epf.pv.ui.common.util.FilteredContentElementOrderList;
 import eu.tanov.epf.pv.ui.techniques.i18n.TechniquesUIResources;
 import eu.tanov.epf.pv.ui.techniques.provider.TechniquesCategoryItemProvider;
+import eu.tanov.epf.pv.ui.techniques.util.TechniquesHelper;
 
 public class TechniqueTasksPage extends AbstractCustomCategoryPage<Task> {
 	private static final String FORM_PAGE_ID = "techniqueTasksPage"; //$NON-NLS-1$	
@@ -45,4 +49,19 @@ public class TechniqueTasksPage extends AbstractCustomCategoryPage<Task> {
 	protected String[] modelStructurePath() {
 		return TechniquesCategoryItemProvider.TECHNIQUES_PATH;
 	}
+
+	@Override
+	protected FilteredContentElementOrderList<Task> createFilteredContentElementOderList() {
+		return new FilteredContentElementOrderList<Task>(contentElement, getOrderFeature(), clazz) {
+			private static final long serialVersionUID = -4411920693746082284L;
+
+			@Override
+			protected List<Task> toFilteredList() {
+				// add work products from tasks.mandatoryInput
+				TechniquesHelper.updateWorkProducts(container);
+				return super.toFilteredList();
+			}
+		};
+	}
+
 }
