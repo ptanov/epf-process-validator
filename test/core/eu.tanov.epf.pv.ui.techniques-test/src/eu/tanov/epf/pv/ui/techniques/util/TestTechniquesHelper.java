5b8d6691ae8af9ca4460071805f603eca412e5a0
Plamen Tanov <1.plamen@gmail.com>
2011-01-02 15:47:57 +0200

    (#47) Added tests for TechniquesHelper.updateWorkProducts()

---
+++ test/core/eu.tanov.epf.pv.ui.techniques-test/src/eu/tanov/epf/pv/ui/techniques/util/TestTechniquesHelper.java
@@ -0,0 +1,138 @@
+package eu.tanov.epf.pv.ui.techniques.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.epf.library.edit.LibraryEditPlugin;
+import org.eclipse.epf.uma.CustomCategory;
+import org.eclipse.epf.uma.DescribableElement;
+import org.eclipse.epf.uma.Task;
+import org.eclipse.epf.uma.UmaFactory;
+import org.eclipse.epf.uma.WorkProduct;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestTechniquesHelper {
+
+	@BeforeClass
+	public static void initPluginSystem() {
+		new LibraryEditPlugin();
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void updateWorkProductsNullTechnique() {
+		TechniquesHelper.updateWorkProducts(null);
+	}
+
+	@Test
+	public void updateWorkProductsAllToAdd() {
+		final CustomCategory technique = UmaFactory.eINSTANCE.createCustomCategory();
+
+		final WorkProduct workProduct1 = UmaFactory.eINSTANCE.createWorkProduct();
+		final WorkProduct workProduct2 = UmaFactory.eINSTANCE.createWorkProduct();
+
+		final Task task = UmaFactory.eINSTANCE.createTask();
+		task.getMandatoryInput().add(workProduct1);
+		task.getMandatoryInput().add(workProduct2);
+
+		technique.getCategorizedElements().add(task);
+
+		final List<DescribableElement> beforeUpdate = technique.getCategorizedElements();
+		assertEquals(1, beforeUpdate.size());
+		assertSame(task, beforeUpdate.get(0));
+
+		TechniquesHelper.updateWorkProducts(technique);
+
+		final List<DescribableElement> afterUpdate = technique.getCategorizedElements();
+		assertEquals(3, afterUpdate.size());
+		assertSame(task, afterUpdate.get(0));
+		assertEqualsIgnoreOrder(afterUpdate.subList(1, afterUpdate.size()), workProduct1, workProduct2);
+	}
+
+	private static void assertEqualsIgnoreOrder(List<?> expectedObjects, Object... actualObjects) {
+		assertEquals(actualObjects.length, expectedObjects.size());
+
+		final ArrayList<Object> remainingExpected = new ArrayList<Object>(expectedObjects);
+
+		for (Object object : actualObjects) {
+			final int indexOf = remainingExpected.indexOf(object);
+			assertTrue("Not found " + object + " in " + expectedObjects, indexOf != -1);
+			remainingExpected.remove(indexOf);
+		}
+	}
+
+	@Test
+	public void updateWorkProductsNothingToAdd() {
+		final CustomCategory technique = UmaFactory.eINSTANCE.createCustomCategory();
+
+		final WorkProduct workProduct1 = UmaFactory.eINSTANCE.createWorkProduct();
+		final WorkProduct workProduct2 = UmaFactory.eINSTANCE.createWorkProduct();
+
+		final Task task = UmaFactory.eINSTANCE.createTask();
+		task.getMandatoryInput().add(workProduct1);
+		task.getMandatoryInput().add(workProduct2);
+
+		technique.getCategorizedElements().add(workProduct1);
+		technique.getCategorizedElements().add(workProduct2);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1, workProduct2));
+
+		technique.getCategorizedElements().add(task);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1, workProduct2, task));
+
+		TechniquesHelper.updateWorkProducts(technique);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1, workProduct2, task));
+	}
+
+	@Test
+	public void updateWorkProductsNotAllAdded() {
+		final CustomCategory technique = UmaFactory.eINSTANCE.createCustomCategory();
+
+		final WorkProduct workProduct1 = UmaFactory.eINSTANCE.createWorkProduct();
+		final WorkProduct workProduct2 = UmaFactory.eINSTANCE.createWorkProduct();
+
+		final Task task = UmaFactory.eINSTANCE.createTask();
+		task.getMandatoryInput().add(workProduct1);
+		task.getMandatoryInput().add(workProduct2);
+
+		technique.getCategorizedElements().add(workProduct1);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1));
+
+		technique.getCategorizedElements().add(task);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1, task));
+
+		TechniquesHelper.updateWorkProducts(technique);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(workProduct1, task, workProduct2));
+	}
+
+	@Test
+	public void updateWorkProductsSkipOptionalInput() {
+		final CustomCategory technique = UmaFactory.eINSTANCE.createCustomCategory();
+
+		final WorkProduct workProduct1 = UmaFactory.eINSTANCE.createWorkProduct();
+		final WorkProduct workProduct2 = UmaFactory.eINSTANCE.createWorkProduct();
+
+		final Task task = UmaFactory.eINSTANCE.createTask();
+		task.getMandatoryInput().add(workProduct1);
+		task.getOptionalInput().add(workProduct2);
+
+		technique.getCategorizedElements().add(task);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(task));
+
+		TechniquesHelper.updateWorkProducts(technique);
+
+		assertEquals(technique.getCategorizedElements(), Arrays.asList(task, workProduct1));
+	}
+
+}
