d18965619b6e3f59084e187f8fe1f7408f2f796b
Plamen Tanov <1.plamen@gmail.com>
2011-01-08 21:53:49 +0200

    (#31) Added technique icon and text in new child menu http://code.google.com/p/epf-process-validator/issues/detail?id=31, Incomplete

--- core/eu.tanov.epf.pv.ui.techniques/src/eu/tanov/epf/pv/ui/techniques/provider/TechniqueItemProvider.java
+++ core/eu.tanov.epf.pv.ui.techniques/src/eu/tanov/epf/pv/ui/techniques/provider/TechniqueItemProvider.java
@@ -7,9 +7,11 @@ import org.eclipse.epf.library.edit.IDefaultNameSetter;
 import org.eclipse.epf.library.edit.ILibraryItemProvider;
 import org.eclipse.epf.library.edit.IStatefulItemProvider;
 import org.eclipse.epf.library.edit.internal.IListenerProvider;
+import org.eclipse.epf.uma.CustomCategory;
 import org.eclipse.jface.resource.ImageDescriptor;
 
 import eu.tanov.epf.pv.ui.techniques.TechniquesActivator;
+import eu.tanov.epf.pv.ui.techniques.i18n.TechniquesUIResources;
 
 public class TechniqueItemProvider extends org.eclipse.epf.library.edit.category.CustomCategoryItemProvider implements
 		ILibraryItemProvider, IStatefulItemProvider, IDefaultNameSetter, IListenerProvider {
@@ -26,6 +28,23 @@ public class TechniqueItemProvider extends org.eclipse.epf.library.edit.category
 	@Override
 	public ImageDescriptor getImage(Object object) {
 		return TechniquesActivator.getDefault().getImage("full/obj16/Technique"); //$NON-NLS-1$
+	}
+
+	@Override
+	public String getCreateChildText(Object owner, Object feature, Object child, Collection<?> selection) {
+		if (child instanceof CustomCategory) {
+			return TechniquesUIResources.Techniques_child;
+		}
+		throw new IllegalStateException("Unknown child type for technique: " + child);
+	}
+
+	@Override
+	public Object getCreateChildImage(Object owner, Object feature, Object child,
+			@SuppressWarnings("rawtypes") Collection selection) {
+		if (child instanceof CustomCategory) {
+			return TechniquesActivator.getDefault().getImage("full/obj16/Technique"); //$NON-NLS-1$
+		}
+		throw new IllegalStateException("Unknown child type for technique: " + child);
 	}
 
 }
