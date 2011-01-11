cde254506090b161323a2fdc4355558f18797beb
Plamen Tanov <1.plamen@gmail.com>
2011-01-11 18:32:41 +0200

    (#70) Added sample constraint

---
+++ validators/eu.tanov.epf.pv.validators.example/src/eu/tanov/epf/pv/validators/example/SampleConstraint.java
@@ -0,0 +1,24 @@
+package eu.tanov.epf.pv.validators.example;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+
+public class SampleConstraint extends AbstractModelConstraint {
+
+	@Override
+	public IStatus validate(IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+		if (!(target instanceof org.eclipse.epf.uma.ProcessComponent)) {
+			return ctx.createSuccessStatus();
+		}
+		final String name = ((org.eclipse.epf.uma.ProcessComponent) target).getName();
+		if ("goodName".equals(name)) {
+			return ctx.createSuccessStatus();
+		} else {
+			return ctx.createFailureStatus(name);
+		}
+	}
+
+}
