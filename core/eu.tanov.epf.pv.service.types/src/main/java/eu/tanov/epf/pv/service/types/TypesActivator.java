21090c83a601edd7f3834a6a50049618c04899ec
Plamen Tanov <1.plamen@gmail.com>
2011-01-20 21:01:29 +0200

    (#109) Project for CustomTypeHandlersService created with name: eu.tanov.epf.pv.service.types

---
+++ core/eu.tanov.epf.pv.service.types/src/main/java/eu/tanov/epf/pv/service/types/TypesActivator.java
@@ -0,0 +1,30 @@
+package eu.tanov.epf.pv.service.types;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class TypesActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		TypesActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		TypesActivator.context = null;
+	}
+
+}
