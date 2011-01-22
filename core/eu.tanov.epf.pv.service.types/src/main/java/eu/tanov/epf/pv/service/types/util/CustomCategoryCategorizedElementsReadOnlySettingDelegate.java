78d1e6bad5f38e8779be85cefa2ca1c3c84e139f
Plamen Tanov <1.plamen@gmail.com>
2011-01-22 09:30:40 +0200

    (#90) Added CustomCategoryCategorizedElementsReadOnlySettingDelegate to handle common cases with CustomCategory.categorizedElements http://code.google.com/p/epf-process-validator/issues/detail?id=90, Complete

---
+++ core/eu.tanov.epf.pv.service.types/src/main/java/eu/tanov/epf/pv/service/types/util/CustomCategoryCategorizedElementsReadOnlySettingDelegate.java
@@ -0,0 +1,43 @@
+package eu.tanov.epf.pv.service.types.util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EStructuralFeature.Internal.DynamicValueHolder;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.epf.uma.CustomCategory;
+import org.eclipse.epf.uma.DescribableElement;
+
+public class CustomCategoryCategorizedElementsReadOnlySettingDelegate<T extends DescribableElement> extends
+		AbstractReadOnlySettingDelegate {
+
+	private final Class<T> clazz;
+
+	public CustomCategoryCategorizedElementsReadOnlySettingDelegate(Class<T> clazz) {
+		if (clazz == null) {
+			throw new NullPointerException("Class should not be null");
+		}
+		this.clazz = clazz;
+	}
+
+	@Override
+	public List<T> dynamicGet(InternalEObject owner, DynamicValueHolder settings, int dynamicFeatureID, boolean resolve,
+			boolean coreType) {
+		if (!(owner instanceof CustomCategory)) {
+			throw new IllegalArgumentException("Holder object is not CustomCategory: " + owner);
+		}
+		final List<DescribableElement> categorizedElements = ((CustomCategory) owner).getCategorizedElements();
+
+		final List<T> result = new LinkedList<T>();
+
+		for (DescribableElement next : categorizedElements) {
+			if (clazz.isInstance(next)) {
+				@SuppressWarnings("unchecked")
+				final T casted = (T) next;
+				result.add(casted);
+			}
+		}
+		return result;
+	}
+
+}
