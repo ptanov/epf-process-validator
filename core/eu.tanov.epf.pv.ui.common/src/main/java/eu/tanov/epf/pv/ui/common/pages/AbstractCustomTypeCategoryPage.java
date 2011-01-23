a09925f59e4b79798bdbd114e9d23040b610fb90
Plamen Tanov <1.plamen@gmail.com>
2011-01-23 19:01:08 +0200

    (#72) Removed unused method (same as parent) http://code.google.com/p/epf-process-validator/issues/detail?id=72, Incomplete

--- core/eu.tanov.epf.pv.ui.common/src/main/java/eu/tanov/epf/pv/ui/common/pages/AbstractCustomTypeCategoryPage.java
+++ core/eu.tanov.epf.pv.ui.common/src/main/java/eu/tanov/epf/pv/ui/common/pages/AbstractCustomTypeCategoryPage.java
@@ -1,8 +1,5 @@
 package eu.tanov.epf.pv.ui.common.pages;
 
-import org.eclipse.epf.authoring.ui.filters.ContentFilter;
-import org.eclipse.epf.library.edit.IFilter;
-import org.eclipse.epf.uma.ContentElement;
 import org.eclipse.epf.uma.DescribableElement;
 import org.eclipse.ui.forms.editor.FormEditor;
 
@@ -18,19 +15,6 @@ public abstract class AbstractCustomTypeCategoryPage<T extends DescribableElemen
 			String classNameForFormTitle) {
 		super(editor, pageId, title, typeHandler.getHolderType(), classNameForFormTitle);
 		this.typeHandler = typeHandler;
-	}
-
-	@Override
-	protected IFilter getFilter() {
-		return filter = new ContentFilter() {
-			protected boolean childAccept(Object obj) {
-				if (obj instanceof ContentElement) {
-					if (getHelper().isContributor((ContentElement) obj))
-						return false;
-				}
-				return clazz.isInstance(obj);
-			}
-		};
 	}
 
 	@Override
