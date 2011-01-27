d321a3578cde9c704fe3d8cb6c5a45452a647103
Plamen Tanov <1.plamen@gmail.com>
2011-01-28 01:21:01 +0200

    (#121) Selection is wrapped to corresponding type http://code.google.com/p/epf-process-validator/issues/detail?id=121, Incomplete

--- core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/WizardsActivator.java
+++ core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/WizardsActivator.java
@@ -2,6 +2,7 @@ package eu.tanov.epf.pv.ui.wizards;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -13,6 +14,8 @@ public class WizardsActivator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static WizardsActivator plugin;
+
+	private BundleContext context;
 	
 	/**
 	 * The constructor
@@ -27,6 +30,8 @@ public class WizardsActivator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		
+		this.context = context;
 	}
 
 	/*
@@ -34,6 +39,8 @@ public class WizardsActivator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+		this.context = null;
+
 		plugin = null;
 		super.stop(context);
 	}
@@ -45,6 +52,20 @@ public class WizardsActivator extends AbstractUIPlugin {
 	 */
 	public static WizardsActivator getDefault() {
 		return plugin;
+	}
+
+	public <T> T getService(Class<T> serviceClass) {
+		final ServiceReference serviceReference = context.getServiceReference(serviceClass.getName());
+		if (serviceReference != null) {
+			@SuppressWarnings("unchecked")
+			final T result = (T) context.getService(serviceReference);
+
+			if (result != null) {
+				return result;
+			}
+		}
+
+		throw new IllegalArgumentException("Service not found: " + serviceClass.getName());
 	}
 
 }
--- core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/action/CreateSimpleValidationConstraintAction.java
+++ core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/action/CreateSimpleValidationConstraintAction.java
@@ -1,13 +1,17 @@
 package eu.tanov.epf.pv.ui.wizards.action;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 
+import eu.tanov.epf.pv.service.types.service.CustomTypeHandlersService;
+import eu.tanov.epf.pv.ui.wizards.WizardsActivator;
 import eu.tanov.epf.pv.ui.wizards.wizard.SelectWizardWizard;
 
 public class CreateSimpleValidationConstraintAction implements IObjectActionDelegate {
@@ -20,10 +24,21 @@ public class CreateSimpleValidationConstraintAction implements IObjectActionDele
 			throw new IllegalStateException("CreateSimpleValidationConstraintAction: selection is null");
 		}
 		final SelectWizardWizard wizard = new SelectWizardWizard();
-		wizard.init(PlatformUI.getWorkbench(), selection);
+		wizard.init(PlatformUI.getWorkbench(), wrapSelection(selection));
 		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell(), wizard);
 		dialog.create();
 		dialog.open();
+	}
+
+	private static IStructuredSelection wrapSelection(IStructuredSelection selection) {
+		final CustomTypeHandlersService service = WizardsActivator.getDefault().getService(CustomTypeHandlersService.class);
+		final Object firstElement = selection.getFirstElement();
+		if (!(firstElement instanceof EObject)) {
+			throw new IllegalStateException("Selection should be from EObjects, not " + firstElement);
+		}
+		final EObject wrapped = service.wrapObjectIfNeeded((EObject) firstElement);
+
+		return new StructuredSelection(wrapped);
 	}
 
 	@Override
