54a7704a20784abc29cdd3e69f3b1841d71f8e17
Plamen Tanov <1.plamen@gmail.com>
2011-01-27 23:50:40 +0200

    (#119) Added StructuralFeaturesTree http://code.google.com/p/epf-process-validator/issues/detail?id=119, Incomplete

--- core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/page/SelectFeaturePage.java
+++ core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/page/SelectFeaturePage.java
@@ -1,10 +1,14 @@
 package eu.tanov.epf.pv.ui.wizards.page;
 
+import org.eclipse.epf.uma.UmaPackage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+
+import eu.tanov.epf.pv.ui.wizards.widgets.StructuralFeaturesTree;
 
 public class SelectFeaturePage extends WizardPage {
 
@@ -18,11 +22,27 @@ public class SelectFeaturePage extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(2, false));
+		Label sportsLabel = new Label(composite, SWT.NONE);
+		sportsLabel.setText("Select feature:");
+		final GridData gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		sportsLabel.setLayoutData(gridData);
+
+		final GridData gridData2 = new GridData();
+		gridData2.horizontalSpan = 2;
+		gridData2.horizontalAlignment = SWT.FILL;
+		gridData2.grabExcessHorizontalSpace = true;
+		gridData2.verticalAlignment = SWT.FILL;
+		gridData2.grabExcessVerticalSpace = true;
+
 		GridLayout gl = new GridLayout();
-		int ncol = 4;
+		int ncol = 2;
 		gl.numColumns = ncol;
-		composite.setLayout(gl);
-		new Label(composite, SWT.NONE).setText("label");
+		final StructuralFeaturesTree tree = new StructuralFeaturesTree(composite);
+		tree.getTree().setLayoutData(gridData2);
+
+		tree.setInput(UmaPackage.eINSTANCE.getDiscipline());
 		setControl(composite);
 	}
 
---
+++ core/eu.tanov.epf.pv.ui.wizards/src/main/java/eu/tanov/epf/pv/ui/wizards/widgets/StructuralFeaturesTree.java
@@ -0,0 +1,119 @@
+package eu.tanov.epf.pv.ui.wizards.widgets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+
+public class StructuralFeaturesTree extends TreeViewer {
+
+	private final IContentProvider contentProvider = new ITreeContentProvider() {
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			return getChildren(inputElement);
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			if (parentElement instanceof EReference) {
+				parentElement = ((EReference) parentElement).getEReferenceType();
+			}
+			if (parentElement instanceof EClass) {
+				final EClass eObject = (EClass) parentElement;
+				final List<EReference> result = new ArrayList<EReference>(eObject.getEStructuralFeatures().size());
+				for (EStructuralFeature object : eObject.getEStructuralFeatures()) {
+					if (object instanceof EReference && (((EReference) object).isMany())) {
+						// add only list EReferences
+						result.add((EReference) object);
+					}
+				}
+				return result.toArray();
+			}
+			return new Object[0];
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			if (element instanceof EObject) {
+				final EObject eObject = (EObject) element;
+				return eObject.eContainer();
+
+			}
+			throw new IllegalArgumentException("getParent(): Only EObjects are supported, not: " + element);
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			return getChildren(element).length > 0;
+		}
+
+	};
+	private final ILabelProvider labelProvider = new ILabelProvider() {
+		public String getText(Object object) {
+			if (!(object instanceof EReference)) {
+				throw new IllegalArgumentException("Only EReference is supported, but was: " + object);
+			}
+			final EReference reference = (EReference) object;
+			return String.format("%s:%s", reference.getName(), reference.getEReferenceType().getName());
+		}
+
+		@Override
+		public void addListener(ILabelProviderListener listener) {
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+
+		@Override
+		public void removeListener(ILabelProviderListener listener) {
+		}
+
+		@Override
+		public Image getImage(Object element) {
+			return null;
+		}
+	};
+
+	public StructuralFeaturesTree(Composite parent) {
+		super(parent, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+
+		setLabelProvider(labelProvider);
+		setContentProvider(contentProvider);
+		setUseHashlookup(true);
+
+		// Set the input to treeViewer
+		// treeViewer.setInput(getInput());
+
+		// treeViewer.addSelectionChangedListener(this);
+		// treeViewer.addDoubleClickListener(this);
+		getTree().setFont(parent.getFont());
+
+	}
+
+}
