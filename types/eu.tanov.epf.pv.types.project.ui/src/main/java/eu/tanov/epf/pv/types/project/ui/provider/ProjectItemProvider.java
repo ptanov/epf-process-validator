90368c4d3985f0512f9f640aa2aa2f57c8f1dadc
Plamen Tanov <1.plamen@gmail.com>
2011-02-05 22:58:38 +0200

    (#168) Iteration adapter added when displaying iteration in library view http://code.google.com/p/epf-process-validator/issues/detail?id=168, Complete

--- types/eu.tanov.epf.pv.types.project.ui/src/main/java/eu/tanov/epf/pv/types/project/ui/provider/ProjectItemProvider.java
+++ types/eu.tanov.epf.pv.types.project.ui/src/main/java/eu/tanov/epf/pv/types/project/ui/provider/ProjectItemProvider.java
@@ -7,9 +7,11 @@ import org.eclipse.epf.library.edit.IDefaultNameSetter;
 import org.eclipse.epf.library.edit.ILibraryItemProvider;
 import org.eclipse.epf.library.edit.IStatefulItemProvider;
 import org.eclipse.epf.library.edit.internal.IListenerProvider;
+import org.eclipse.epf.uma.CustomCategory;
 import org.eclipse.swt.graphics.Image;
 
 import eu.tanov.epf.pv.types.project.ui.ProjectActivator;
+import eu.tanov.epf.pv.types.projectiteration.ui.provider.ProjectIterationItemProvider;
 
 public class ProjectItemProvider extends org.eclipse.epf.library.edit.category.CustomCategoryItemProvider implements
 		ILibraryItemProvider, IStatefulItemProvider, IDefaultNameSetter, IListenerProvider {
@@ -20,7 +22,31 @@ public class ProjectItemProvider extends org.eclipse.epf.library.edit.category.C
 
 	@Override
 	public Collection<?> getChildren(Object object) {
-		return super.getChildren(object);
+		// TODO why not using standard scheme?
+		if (!(object instanceof CustomCategory)) {
+			throw new IllegalArgumentException("Only custom categories expected, not: " + object);
+		}
+		final CustomCategory category = ((CustomCategory) object);
+		final Collection<?> children = category.getCategorizedElements();
+
+		for (Object next : children) {
+			if (next instanceof CustomCategory) {
+				final CustomCategory customCategory = (CustomCategory) next;
+
+				final ProjectIterationItemProvider itemProvider = new ProjectIterationItemProvider(adapterFactory);
+				customCategory.eAdapters().add(itemProvider);
+
+				// TODO issue #56 use adapter factory extension
+				// final ILibraryItemProvider itemProvider = (ILibraryItemProvider) TngUtil
+				// .getBestAdapterFactory(adapterFactory).adapt(customCategory,
+				// ITreeItemContentProvider.class);
+
+				itemProvider.setParent(object);
+			} else {
+				throw new IllegalStateException(String.format("Not custom category in project: %s, but: %s", object, next));
+			}
+		}
+		return children;
 	}
 
 	@Override
